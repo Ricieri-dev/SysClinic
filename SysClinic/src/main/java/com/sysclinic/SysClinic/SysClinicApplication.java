84a4b55af75ff2b87867705bff7459339ca81809
Filipe Ricieri - TI <ti@budini.com>
2026-05-12 17:25:58 -0300

    feat:  estrutura inicial do projeto

---
+++ SysClinic/src/main/java/com/sysclinic/SysClinic/SysClinicApplication.java
@@ -0,0 +1,13 @@
+package com.sysclinic.SysClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysClinicApplication.class, args);
+	}
+
+}
---
+++ SysClinic/src/test/java/com/sysclinic/SysClinic/SysClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysclinic.SysClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
